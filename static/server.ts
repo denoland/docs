fe013f17811e380173b8a13e29f11a53c50d7864
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-08-29 00:37:51 -0500

    add manual redirects

--- static/server.ts
+++ static/server.ts
@@ -2,9 +2,23 @@ import { Hono } from "https://deno.land/x/hono@v3.5.5/mod.ts";
 import { serveStatic } from "https://deno.land/x/hono@v3.5.5/middleware.ts";
 
 const app = new Hono();
-app.use("*", serveStatic({ root: "./" }));
-app.use("*", serveStatic({ root: "./", path: "./404.html" }));
 
-app.get("/", (c) => c.text("Hello Deno!"));
+// Configure redirects
+app.get("/manual", (c) => c.redirect("/runtime/manual"));
+app.get("/runtime", (c) => c.redirect("/runtime/manual"));
+app.get("/deploy", (c) => c.redirect("/deploy/manual"));
+app.get("/deploy/docs", (c) => c.redirect("/deploy/manual"));
+
+// Redirect all manual paths - most should work
+app.all("/manual.*", (c) => {
+  const unversionedPath = c.req.path.split("/").slice(2);
+  return c.redirect("/runtime/manual/" + unversionedPath.join("/"));
+});
+
+// Static site
+app.use("*", serveStatic({ root: "./" }));
+
+// 404s
+app.use("*", serveStatic({ root: "./", path: "./404.html" }));
 
 Deno.serve(app.fetch);
