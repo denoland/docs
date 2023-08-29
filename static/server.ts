5d67a81bae5c5cc1b65b8eb17bcd25c9f3da47ee
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-08-29 08:45:02 -0500

    redirects for deploy docs

--- static/server.ts
+++ static/server.ts
@@ -15,10 +15,54 @@ app.all("/manual.*", (c) => {
   return c.redirect("/runtime/manual/" + unversionedPath.join("/"));
 });
 
+// Redirect all deploy docs paths
+[
+  "discord-slash",
+  "fresh",
+  "simple-api",
+  "tutorial-blog-fresh",
+  "tutorial-dynamodb",
+  "tutorial-faunadb",
+  "tutorial-firebase",
+  "tutorial-http-server",
+  "tutorial-hugo-blog",
+  "tutorial-postgres",
+  "tutorial-wordpress-frontend",
+  "vite",
+].forEach((slug) => {
+  app.get(
+    `/deploy/docs/${slug}`,
+    (c) => c.redirect(`/deploy/tutorials/${slug}`),
+  );
+});
+
+[
+  "compression",
+  "runtime-broadcast-channel",
+  "runtime-fetch",
+  "runtime-fs",
+  "runtime-headers",
+  "runtime-node",
+  "runtime-request",
+  "runtime-response",
+  "runtime-sockets",
+].forEach((slug) => {
+  app.get(
+    `/deploy/docs/${slug}`,
+    (c) => c.redirect(`/deploy/api/${slug}`),
+  );
+});
+
+app.all("/deploy/docs.*", (c) => {
+  const unversionedPath = c.req.path.split("/").slice(3);
+  return c.redirect("/deploy/manual/" + unversionedPath.join("/"));
+});
+
 // Static site
 app.use("*", serveStatic({ root: "./" }));
 
 // 404s
 app.use("*", serveStatic({ root: "./", path: "./404.html" }));
 
+// Serve on port 8000
 Deno.serve(app.fetch);
