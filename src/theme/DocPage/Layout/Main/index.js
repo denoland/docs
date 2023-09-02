ded20b5a9bcf445e7cf2516276e664b432e7196d
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-09-02 09:56:38 -0500

    include footer in main page content

---
+++ src/theme/DocPage/Layout/Main/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useDocsSidebar} from '@docusaurus/theme-common/internal';
+import styles from './styles.module.css';
+import Footer from '@theme/Footer';
+
+export default function DocPageLayoutMain({hiddenSidebarContainer, children}) {
+  const sidebar = useDocsSidebar();
+  return (
+    <main
+      className={clsx(
+        styles.docMainContainer,
+        (hiddenSidebarContainer || !sidebar) && styles.docMainContainerEnhanced,
+      )}>
+      <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+        <div
+          className={clsx(
+            'container padding-top--md padding-bottom--lg',
+            styles.docItemWrapper,
+            hiddenSidebarContainer && styles.docItemWrapperEnhanced,
+          )}>
+          {children}
+        </div>
+        <Footer />
+      </div>
+    </main>
+  );
+}
