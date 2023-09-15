688901eecda861a179249fed87a6f74a98ecee28
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-09-15 07:09:22 -0500

    track 404s in KV

--- src-deno/server.ts
+++ src-deno/server.ts
@@ -2,6 +2,7 @@ import { Hono } from "./deps.ts";
 import { serveStatic } from "./deps.ts";
 import configureRedirects from "./redirects.ts";
 
+const kv = await Deno.openKv();
 const app = new Hono();
 
 // Configure redirects
@@ -12,7 +13,14 @@ app.use("*", serveStatic({ root: "./" }));
 
 // 404s
 app.notFound(async (c) => {
-  console.error("404 error returned for path: ", c.req.path);
+  try {
+    await kv.set(["404s", c.req.path], c.req.path);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    console.error("404 error returned for path: ", c.req.path);
+  }
+
   const f = await Deno.readTextFile("./404.html");
   c.status(404);
   return c.html(f);
