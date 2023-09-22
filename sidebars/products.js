7972e468560598f29aab9eb10ec0d9afd683580c
hashrock <hashrock@users.noreply.github.com>
2023-09-23 03:35:28 +0900

    fix: Replace "Back to main menu" navigation on mobile Navbar to provide clearer context (#137)
    Co-authored-by: Kevin Whinnery <kevin.whinnery@gmail.com>

---
+++ sidebars/products.js
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: "Deno Runtime",
+    shortName: "Runtime",
+    slug: "runtime",
+  },
+  {
+    name: "Deno Deploy",
+    shortName: "Deploy",
+    slug: "deploy",
+  },
+  {
+    name: "Deno KV",
+    shortName: "KV",
+    slug: "kv",
+  },
+];
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,43 +1,54 @@
 import React from "react";
 import Layout from "@theme/Layout";
 
+function Card({ title, href, children }) {
+  return (
+    <div className="border-1 border-solid rounded-xl p-4 w-full md:w-auto flex flex-col">
+      <h3 className="border-b border-t-0 border-l-0 border-r-0 border-solid pb-2">
+        {title}
+      </h3>
+      <p className="grow">{children}</p>
+      <a
+        className="font-bold inline-block hover:opacity-80"
+        href={href}
+      >
+        {title} Docs &gt;
+      </a>
+    </div>
+  );
+}
+
 export default function Home() {
-  const ctaLinks =
-    `border-2 border-solid rounded-xl px-4 py-2 hover:no-underline font-bold inline-block hover:opacity-80`;
   return (
     <Layout
-      title={`Deno: the easiest, most secure JavaScript runtime`}
+      title={"Deno: the easiest, most secure JavaScript runtime"}
       description="Reference documentation for the Deno runtime and Deno Deploy"
     >
-      <div className="flex flex-col-reverse px-8 pt-6 md:mt-12 md:items-center md:justify-center md:flex-row gap-0 md:gap-16">
+      <div className="flex flex-col px-8 pt-6 md:mt-12 md:items-center md:justify-center md:flex-row gap-0 md:gap-16 max-w-[1200px] mx-auto">
         <div className="pb-16 align-middle md:pb-0">
-          <div className="mb-8 md:mb-12">
-            <h1 className="text-4xl md:text-5xl">Deno Documentation</h1>
-            <p className="my-2">
-              Reference docs for the Deno JavaScript runtime and Deno Deploy.
-            </p>
+          <div className="mb-8 text-center">
+            <img
+              className="w-64 h-64 mb-[-40px] md:mt-[-40px]"
+              alt="Deno logo"
+              src="/deno-looking-up.svg"
+            />
+            <h1 className="text-4xl md:text-5xl">Deno Docs</h1>
           </div>
-          <div className="flex flex-col items-start gap-4 md:flex-row">
-            <a
-              className={ctaLinks}
-              href="/runtime/manual"
-            >
-              Build with Deno
-            </a>
-            <a
-              className={ctaLinks}
-              href="/deploy/manual"
-            >
-              Deploy to the edge
-            </a>
+          <div className="flex flex-col items-start gap-8 md:grid md:grid-cols-3 md:grid-flow-col md:items-stretch">
+            <Card title="Deno Runtime" href="/runtime/manual">
+              Language runtime for TypeScript and JavaScript with a browser-like
+              programming environment. Features built-in dev tools, powerful
+              platform APIs, and native support for TypeScript and JSX.
+            </Card>
+            <Card title="Deno Deploy" href="/deploy/manual">
+              Serverless JavaScript platform. Supports Deno platform APIs and
+              Node.js / npm modules. Runs on a fast global edge network.
+            </Card>
+            <Card title="Deno KV" href="/kv/manual">
+              Key/value database built in to the Deno runtime. Simple API, works
+              with zero configuration on Deno Deploy.
+            </Card>
           </div>
-        </div>
-        <div className="text-center align-middle">
-          <img
-            className="w-64 h-64 md:h-96 md:w-96 lg:h-[520px] lg:w-[520px]"
-            alt="Deno logo"
-            src="/deno-looking-up.svg"
-          />
         </div>
       </div>
     </Layout>
