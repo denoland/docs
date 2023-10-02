a01ce693e42b49d9446c48b29d411034f307d5b0
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2023-10-02 10:22:18 -0500

    Customize docs breadcrumbs (#152)
    Co-authored-by: Kevin Whinnery <kevin.whinnery@gmail.com>

---
+++ src/theme/DocBreadcrumbs/Items/Home/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Link from "@docusaurus/Link";
+import {translate} from "@docusaurus/Translate";
+import {useLocation} from "@docusaurus/router";
+
+export default function HomeBreadcrumbItem() {
+  const currentPath = useLocation().pathname
+  const getDocsLocation = () => {
+    if (currentPath.startsWith("/deploy")) {
+      return {
+        href: "/deploy/manual",
+        name: "Deploy",
+      }
+    } else if (currentPath.startsWith("/kv")) {
+      return {
+        href: "/kv/manual",
+        name: "KV",
+      }
+    } else if (currentPath.startsWith("/runtime")) {
+      return {
+        href: "/runtime/manual",
+        name: "Runtime",
+      }
+    }
+    return {
+      href: "/",
+      name: "All docs"
+    }
+  };
+
+  const {href, name} = getDocsLocation()
+  return (
+    <li className="breadcrumbs__item">
+      <Link
+        aria-label={translate({
+          id: 'theme.docs.breadcrumbs.home',
+          message: 'Home page',
+          description: 'The ARIA label for the home page in the breadcrumbs',
+        })}
+        className="breadcrumbs__link"
+        href={href}>
+        {name}
+      </Link>
+    </li>
+  );
+}
