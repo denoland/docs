4ba6f2fd8cdafc435d574d6eb0b649d1ff400acb
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-12-06 17:47:29 -0600

    remove unneeded component code

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -25,10 +25,6 @@ export default function Layout(props) {
   } = props;
   useKeyboardNavigation();
 
-  window.TrackJS && TrackJS.install({ 
-    token: "abb73baafbd94b74878a59fc03e5ac1b"
-  });
-
   return (
     <LayoutProvider>
       <Head>
