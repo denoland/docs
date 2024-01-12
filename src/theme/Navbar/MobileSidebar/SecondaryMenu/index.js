e887700c6df3f734f1caf2bd7339613b83230080
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2024-01-12 14:50:14 -0600

    Unstyle search bar and make main docs button more clearly a back button (#268)

--- src/theme/Navbar/MobileSidebar/SecondaryMenu/index.js
+++ src/theme/Navbar/MobileSidebar/SecondaryMenu/index.js
@@ -9,8 +9,9 @@ function SecondaryMenuBackButton(props) {
     <button
       type="button"
       {...props}
-      className="secondary-menu__product__button"
+      className="secondary-menu__product__button [font:inherit]"
     >
+			<span aria-hidden="true" className="inline-block transform rotate-180 [font-family:inherit]">&ensp;-&gt; </span>
       All docs
     </button>
   );
