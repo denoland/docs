8e0ad111458d05ef42aaa8b8573b20dddacbb54e
Kevin Whinnery <kevin.whinnery@gmail.com>
2024-01-29 18:11:06 -0600

    add diff syntax highlighting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -233,6 +233,7 @@ const config = {
     prism: {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
+      additionalLanguages: ["powershell", "diff"],
     },
     algolia: {
       // The application ID provided by Algolia
