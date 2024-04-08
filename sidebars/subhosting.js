a4d79c2658b2823156c5833bd5b4982789063dd1
Arnau Orriols <arnauorriolsmiro@gmail.com>
2024-04-08 14:27:27 +0200

    Add to menu

--- sidebars/subhosting.js
+++ sidebars/subhosting.js
@@ -8,6 +8,7 @@ const sidebars = {
     "manual/index",
     "manual/getting_started",
     "manual/projects_and_deployments",
+    "manual/events"
     {
       type: "html",
       value: "<div>REST API</div>",
