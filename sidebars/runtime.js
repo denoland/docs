3dd749b2503b8f8e9aedf1e5a3fb0a465c4aa8d9
Jo Franchetti <jofranchetti@gmail.com>
2024-04-08 14:50:34 +0100

    add docs for coverage command

--- sidebars/runtime.js
+++ sidebars/runtime.js
@@ -127,6 +127,11 @@ const sidebars = {
         },
         {
           type: "doc",
+          label: "deno coverage",
+          id: "manual/tools/coverage",
+        },
+        {
+          type: "doc",
           label: "deno doc",
           id: "manual/tools/documentation_generator",
         },
