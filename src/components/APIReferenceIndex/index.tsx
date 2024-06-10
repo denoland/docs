d2169a02c41f7040544c03f1bf650ca827de9b25
John Donmoyer <john@deno.com>
2024-06-10 15:56:50 -0400

    add index page styles

--- src/components/APIReferenceIndex/index.tsx
+++ src/components/APIReferenceIndex/index.tsx
@@ -20,19 +20,28 @@ const APIReferenceIndex: React.FC<APIReferenceIndexProps> = ({ apiLinks }) => {
       description="Reference documentation for the Deno runtime and Deno Deploy"
     >
       <div className="w-full mt-8 h-screen max-w-screen-xl mx-auto">
-        <div className="mt-24">
-          <ul>
+        <div className="max-w-prose flex flex-col gap-8 mt-32">
+          <header>
+            <h1 className="text-3xl mb-2">Deno API Reference</h1>
+            <div
+              role="doc-subtitle"
+              className="text-gray-500 dark:text-gray-0 text-lg"
+            >
+              Description about Deno APIs. These are APIs you can use with Deno.
+            </div>
+          </header>
+          <ul className="pl-0">
             {apiLinks.map((link) => {
               return (
-                <li className="list-none" key={link.id}>
+                <li className="list-none flex flex-col gap-1" key={link.id}>
                   <a
                     href={link.url}
-                    className="text-inherit underline decoration-gray-300 dark:decoration-gray-600"
+                    className="text-inherit text-lg underline underline-offset-4 decoration-gray-300 dark:decoration-gray-600"
                     title={link.description}
                   >
                     {link.title}
                   </a>
-                  <p className="text-gray-500 dark:text-gray-0">
+                  <p className="text-gray-500 dark:text-gray-0 leading-normal">
                     {link.description}
                   </p>
                 </li>
