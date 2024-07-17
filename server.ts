9a5b67d6c46f8e3429453ef9ff40a1a2024ad57c
crowlkats <crowlkats@toaxl.com>
2024-07-17 05:05:14 +0200

    feat: re-introduce GA

--- server.ts
+++ server.ts
@@ -1,5 +1,13 @@
 import Server from "lume/core/server.ts";
 import REDIRECTS from "./_redirects.json" with { type: "json" };
+import {
+  type Event,
+  formatStatus,
+  GA4Report,
+  isDocument,
+  isRedirect,
+  isServerError,
+} from "ga4";
 
 const server = new Server({
   port: 8000,
@@ -8,21 +16,125 @@ const server = new Server({
 
 REDIRECTS["/api/"] = "/api/deno/";
 
-server.use(async (req, next) => {
-  const url = new URL(req.url, "http://localhost:8000");
-  const redirect = REDIRECTS[url.pathname] || REDIRECTS[url.pathname + "/"];
-  if (redirect) {
-    return new Response(null, {
-      status: 301,
-      headers: {
-        "Location": redirect,
+const GA4_MEASUREMENT_ID = Deno.env.get("GA4_MEASUREMENT_ID");
+function ga4(
+  request: Request,
+  remoteAddr: Deno.Addr,
+  response: Response,
+  error?: unknown,
+) {
+  Promise.resolve().then(async () => {
+    if (request.method !== "GET") {
+      return;
+    }
+
+    if (!isDocument(request, response) && error == null) {
+      return;
+    }
+
+    let event: Event | null = null;
+    if (isRedirect(response)) {
+      const redirectLocation = response.headers.get("location");
+      event = { name: "redirect", params: { redirectLocation } };
+    } else {
+      const fetchDest = request.headers.get("sec-fetch-dest");
+      if (fetchDest) {
+        if (/^(document|i?frame|video)$/.test(fetchDest)) {
+          event = { name: "page_view", params: {} };
+        } else {
+          event = null; // Don't report asset downloads.
+        }
+      } else {
+        const contentType = response.headers.get("content-type");
+        if (contentType != null && /text\/html/.test(contentType)) {
+          event = { name: "page_view", params: {} }; // Probably an old browser.
+        } else {
+          event = { name: "file_download", params: {} };
+        }
+      }
+    }
+
+    if (event == null && error == null) {
+      return;
+    }
+
+    if (event != null) {
+      // And add some extra HTTP metadata to the event.
+      event.params.httpRequestMethod = request.method;
+      event.params.httpResponseStatus = formatStatus(response);
+      event.params.clientType = "browser";
+    }
+
+    // If an exception was thrown, build a separate event to report it.
+    let exceptionEvent;
+    if (error != null) {
+      exceptionEvent = {
+        name: "exception",
+        params: {
+          description: String(error),
+          fatal: isServerError(response),
+        },
+      };
+    } else {
+      exceptionEvent = undefined;
+    }
+
+    // Create basic report.
+    const report = new GA4Report({
+      measurementId: GA4_MEASUREMENT_ID,
+      request,
+      response,
+      conn: {
+        remoteAddr,
+        localAddr: server.addr!,
       },
     });
-  } else {
-    return await next(req);
+
+    // Override the default (page_view) event.
+    report.event = event;
+
+    // Add the exception event, if any.
+    if (exceptionEvent != null) {
+      report.events.push(exceptionEvent);
+    }
+
+    await report.send();
+  }).catch((err) => {
+    console.error(`Internal error: ${err}`);
+  });
+}
+
+server.use(async (req, next, info) => {
+  let err;
+  let res: Response;
+  try {
+    const url = new URL(req.url, "http://localhost:8000");
+    const redirect = REDIRECTS[url.pathname] || REDIRECTS[url.pathname + "/"];
+    if (redirect) {
+      res = new Response(null, {
+        status: 301,
+        headers: {
+          "Location": redirect,
+        },
+      });
+    } else {
+      res = await next(req);
+    }
+    return res;
+  } catch (e) {
+    res = new Response("Internal Server Error", {
+      status: 500,
+    });
+    err = e;
+    throw e;
+  } finally {
+    ga4(
+      req,
+      info.remoteAddr,
+      res!,
+      err,
+    );
   }
 });
-
-server.start();
 
 console.log("Listening on http://localhost:8000");
