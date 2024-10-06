99d2a5221da6905d865b041528f82a943b787f64
Yoshiya Hinosawa <stibium121@gmail.com>
2024-10-06 10:46:11 +0100

    limit concurrecy

--- .github/workflows/link_checker.ts
+++ .github/workflows/link_checker.ts
@@ -1,8 +1,8 @@
 import { DOMParser } from "jsr:@b-fuze/deno-dom";
-
-type Element = import("jsr:@b-fuze/deno-dom").Element;
+import RunQueue from "npm:run-queue@2";
 
 const visitedPages = new Set<string>();
+const queue = new RunQueue({ maxConcurrency: 10 });
 
 const rootURL = new URL(Deno.env.get("DOCS_ROOT")!);
 const rootPage = await fetch(rootURL).then((res) => res.text());
@@ -10,55 +10,58 @@ const rootPage = await fetch(rootURL).then((res) => res.text());
 visitedPages.add(rootURL.href);
 let hasInvalidHrefs = false;
 
-await traversePage(rootURL, rootPage);
+traversePage(rootURL, rootPage);
+
+await queue.run();
+
+console.log("Total pages checked:", visitedPages.size);
 
 if (hasInvalidHrefs) {
   Deno.exit(1);
 }
 
-async function traversePage(url: URL, content: string) {
+function traversePage(url: URL, content: string) {
+  console.log("Checking", url.href);
   const doc = new DOMParser().parseFromString(content, "text/html");
-  // TODO(kt3k): Queue tasks and run them in limited concurrency
   for (const a of doc.querySelectorAll("a")) {
-    await checkAnchor(a, url);
+    const href = a.getAttribute("href")?.trim();
+    if (!href) {
+      hasInvalidHrefs = true;
+      console.error(`Empty href on '${url.pathname}': ${a.outerHTML}`);
+      continue;
+    }
+
+    if (
+      href.startsWith("http") || href.startsWith("mailto") ||
+      href.startsWith("vscode:")
+    ) {
+      continue;
+    }
+
+    const aURL = new URL(href, url);
+    if (aURL.pathname.startsWith("/api")) {
+      continue;
+    }
+
+    aURL.hash = "";
+
+    if (visitedPages.has(aURL.href)) {
+      continue;
+    }
+    visitedPages.add(aURL.href);
+    queue.add(0, fetchUrl, [href, aURL, url]);
   }
 }
 
-// deno-lint-ignore require-await
-async function checkAnchor(a: Element, url: URL) {
-  const href = a.getAttribute("href")?.trim();
-  if (!href) {
-    hasInvalidHrefs = true;
-    console.error(`Empty href on '${url.pathname}': ${a.outerHTML}`);
-    return;
-  }
-
-  if (
-    href.startsWith("http") || href.startsWith("mailto:") ||
-    href.startsWith("vscode:")
-  ) {
-    return;
-  }
-
-  const aURL = new URL(href, url);
-  if (aURL.pathname.startsWith("/api")) {
-    return;
-  }
-
-  if (visitedPages.has(aURL.href)) {
-    return;
-  }
-
-  visitedPages.add(aURL.href);
-
-  return fetch(aURL).then((res) => {
+function fetchUrl(href: string, urlToCheck: URL, baseUrl: URL) {
+  return fetch(urlToCheck).then((res) => {
     if (res.status === 200) {
       if (res.headers.get("content-type")?.includes("text/html")) {
-        return res.text().then((text) => traversePage(aURL, text));
+        return res.text().then((text) => traversePage(urlToCheck, text));
       }
     } else {
       hasInvalidHrefs = true;
-      console.error(`${res.status} on '${url.pathname}': ${href}`);
+      console.error(`${res.status} on '${baseUrl.pathname}': ${href}`);
     }
 
     return res.body?.cancel();
