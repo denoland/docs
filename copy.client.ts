0423be5c4ce6103a5fb5469c644d433bf1c2e867
rb-ntnx <rahul.bhooteshwar@gmail.com>
2024-10-15 19:42:51 +0530

    fix(code-copy-plugin): Remove leading "$" or ">"  from commands when copied
    - In many command examples, leading  ">" or "$ "signs are used (although the usage is not consistent)
    - When copy button is used those leading symbols getting copied
    - When copied commands are used by the user, it gives error (it can be frustrating as well).

--- copy.client.ts
+++ copy.client.ts
@@ -2,6 +2,9 @@ const copyBtns = document.querySelectorAll("button[data-copy]");
 
 copyBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    navigator?.clipboard?.writeText(btn.getAttribute("data-copy") as string);
+    let textToCopy = btn.getAttribute("data-copy") as string;
+    // CLEAN COMMANDS:  Remove leading spaces, $, and > from each line
+    textToCopy = textToCopy.replace(/^[\$>\s]+/, "");
+    navigator?.clipboard?.writeText(textToCopy);
   });
 });
