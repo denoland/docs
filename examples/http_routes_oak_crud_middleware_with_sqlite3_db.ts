6cf74b166c4736c9041c3113b2fa3988828681f8
rjs <mailtoraj369@gmail.com>
2024-11-01 12:08:00 +0530

    formatted code after refactoring to meet guideline on line count

--- examples/http_routes_oak_crud_middleware_with_sqlite3_db.ts
+++ examples/http_routes_oak_crud_middleware_with_sqlite3_db.ts
@@ -11,38 +11,39 @@ import { Application, Router } from "jsr:@oak/oak";
 import { Database } from "jsr:@db/sqlite";
 // Open a database from file, creates if doesn't exist. here the 'people.db' is a file-based database
 const peopleDb = new Database("people.db");
-// Create Table "people" if not exists with schema as shown below
-peopleDb.exec("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null,age INTEGER not null)");
+peopleDb.exec(
+  "CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null,age INTEGER not null)",
+);
 const app = new Application();
 const router = new Router();
-const PORT = "8369";
-// Create person record into people database
 router.post("/people", async (ctx) => {
   const { name, age } = await ctx.request.body.json();
-  await peopleDb.prepare("INSERT INTO people (name, age) VALUES (?,?)",).run(name, age);
+  await peopleDb.prepare("INSERT INTO people (name, age) VALUES (?,?)").run(
+    name,
+    age,
+  );
   const lastInsertRowId = await peopleDb.lastInsertRowId; // Get the last inserted row ID
   ctx.response.status = 201;
   ctx.response.body = { id: lastInsertRowId, name, age };
 });
-// Read all records from people database
 router.get("/people", async (ctx) => {
   const users = await peopleDb.prepare("SELECT * FROM people").all();
   ctx.response.body = users;
 });
-// Updates person record in people database (name value is optional)
 router.put("/people/:id", async (ctx) => {
   const { name, age } = await ctx.request.body.json();
-  await peopleDb.prepare("UPDATE people SET name =?, age=? WHERE id = ?",).run(name, age, ctx.params.id);
-  ctx.response.body = { message: "person updated successfully as requested" }
+  await peopleDb.prepare("UPDATE people SET name =?, age=? WHERE id = ?").run(
+    name,
+    age,
+    ctx.params.id,
+  );
+  ctx.response.body = { message: "person updated successfully as requested" };
 });
-// Delete person record from people database
 router.delete("/people/:id", async (ctx) => {
   await peopleDb.prepare("DELETE FROM people WHERE id = ?").run(ctx.params.id);
-  ctx.response.body = "person removed successfully as requested" ;
+  ctx.response.body = "person removed successfully as requested";
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
-console.log(`Server is running on http://localhost:${PORT}`);
-await app.listen({ port: PORT });
-// Export the app instance for testing
-export default app;
+await app.listen({ port: "8369" }); // Any available port number can be defined here
+export default app; // Export the app instance for testing
