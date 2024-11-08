31ae4459622cedb48c54b865b2f3f496e6b3921a
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2024-11-08 15:54:50 -0600

    Update feedback module

--- _components/Feedback.tsx
+++ _components/Feedback.tsx
@@ -4,17 +4,20 @@ export default function Feedback({ file }) {
   } else {
     const githubPath = "https://github.com/denoland/docs/edit/main" + file;
     return (
-      <section class="feedback-form">
-        <h2 class="form-header">Help us make these docs great!</h2>
+      <section class="flex flex-col mt-12 gap-2 p-4 border-2 border-blue-100
+      dark:border-background-tertiary bg-blue-50 dark:bg-background-secondary
+      rounded max-w-[66ch] mx-auto">
+        <h2 class="text-xl border-b border-blue-100 dark:border-background-tertiary
+        mb-2 pb-2 font-normal">
+          Did you find what you needed?
+        </h2>
         <div class="flex flex-col sm:flex-row gap-8">
           <div class="flex-1">
             <form aria-live="polite" id="feedback-form">
-              <h3 class="form-section-header">Did you find what you needed?</h3>
-              <p>
-                We love your feedback! Let us know how we could make this page
-                better.
-              </p>
-              <div role="radiogroup" aria-labelledby="feedback-title">
+              <div
+                aria-labelledby="feedback-title"
+                class="flex flex-wrap w-full items-center gap-2"
+              >
                 <input
                   class="hidden"
                   id="feedback-yes"
@@ -23,7 +26,7 @@ export default function Feedback({ file }) {
                   aria-label="Yes"
                   value="yes"
                 />
-                <label class="btn mr-1" for="feedback-yes">
+                <label class="btn" for="feedback-yes">
                   Yes
                   <svg
                     aria-hidden="true"
@@ -62,16 +65,41 @@ export default function Feedback({ file }) {
                     </path>
                   </svg>
                 </label>
+                <a
+                  class="btn"
+                  target="_blank"
+                  href={githubPath}
+                >
+                  Edit this page
+
+                  <svg
+                    width="98"
+                    height="96"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    focusable="false"
+                    fill="currentColor"
+                    style="display:inline-block;user-select:none;vertical-align:text-bottom;"
+                    viewBox="0 0 98 96"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
                 <div id="feedback-more" class="hidden">
                   <p class="font-semibold !mb-2" role="status">
                     Thank you! Feedback received.
                   </p>
                   <div>
                     <label for="feedback-comment">
-                      Can you tell us more about your rating? (Optional)
+                      Any additional comments? (Optional)
                     </label>
                     <textarea
-                      class="block w-full"
+                      class="block w-full p-2 border border-foreground-tertiary dark:bg-background-primary rounded"
                       name="feedback-comment"
                       id="feedback-comment"
                     >
@@ -79,18 +107,18 @@ export default function Feedback({ file }) {
                   </div>
                   <div>
                     <label for="feedback-contact">
-                      Leave your email if we can contact you.(Optional)
+                      Email address (optional)
                     </label>
                     <input
                       type="email"
-                      class="block w-full"
+                      class="block w-full p-2 border border-foreground-tertiary dark:bg-background-primary rounded"
                       name="feedback-contact"
                       id="feedback-contact"
                       aria-invalid="false"
                       value=""
                     />
                   </div>
-                  <p class="!mt-2 text-sm">
+                  <p class="!mt-2 mb-4 text-sm">
                     If you need a reply, please contact{" "}
                     <a href="https://support.github.com/">support</a>.
                   </p>
@@ -108,48 +136,22 @@ export default function Feedback({ file }) {
                     </label>
                     <button
                       type="submit"
-                      class="mr-3 px-4 py-2 text-sm font-semibold text-background-primary rounded bg-primary hover:bg-primary-dark border border-primary-dark dark:border-background-tertiary transition"
+                      class="btn"
                     >
                       Send
                     </button>
                   </div>
                 </div>
-                <a
-                  rel=""
-                  class="mt-4 underline underline-offset-2 text-xs block"
-                  target="_blank"
-                  href="/deploy/manual/privacy-policy"
-                >
-                  Privacy policy
-                </a>
               </div>
-            </form>
-          </div>
-          <div class="flex-1">
-            <h3 class="form-section-header">Make a contribution</h3>
-            <p class="mb-1">
-              Deno's docs are open source. See something that's wrong or
-              unclear? Submit a pull request:
-            </p>
-            <a
-              class="btn"
-              target="_blank"
-              href={githubPath}
-            >
-              Edit this page
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                viewBox="0 0 16 16"
-                width="16"
-                height="16"
-                fill="currentColor"
-                style="display:inline-block;user-select:none;vertical-align:text-bottom;"
+              <a
+                rel=""
+                class="mt-8 !underline underline-offset-2 text-xs block"
+                target="_blank"
+                href="/deploy/manual/privacy-policy"
               >
-                <path d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z">
-                </path>
-              </svg>
-            </a>
+                Privacy policy
+              </a>
+            </form>
           </div>
         </div>
       </section>
