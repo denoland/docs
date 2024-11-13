8c523b8813d03a8b9b4e50d4193449b7ac75b368
John Donmoyer <john@deno.com>
2024-11-13 15:46:15 -0500

    add banner for self-hosted (#1136)

--- index.page.tsx
+++ index.page.tsx
@@ -352,7 +352,21 @@ export default function () {
               </div>
             </div>
           </div>
-
+          <div class="flex flex-col gap-4 p-4 bg-deploy-50 dark:bg-background-secondary border-l-4 text-deploy-foreground border-deploy-500">
+            <p class="text-lg">
+              Run Deno Deploy on your own infrastructure with our self-hosted
+              solution running on AWS, GCP, or Azure.{"  "}
+              <a
+                href="https://unf275cfh14.typeform.com/to/dIicJYSQ"
+                class="deploy-link underline underline-offset-4 external"
+              >
+                Learn more{" "}
+                <span aria-hidden="true" class="whitespace-pre">
+                  -&gt;
+                </span>
+              </a>
+            </p>
+          </div>
           {/* Deploy content */}
           <div class="flex flex-col gap-8">
             {/* Scection Header */}
