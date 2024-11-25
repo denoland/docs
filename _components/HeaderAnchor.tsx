4a507a6ac3cd4c3205ce246a0e4853ba1411e132
Jo Franchetti <jofranchetti@gmail.com>
2024-11-25 11:00:36 +0000

    move components into separate files  (#1190)

---
+++ _components/HeaderAnchor.tsx
@@ -0,0 +1,13 @@
+export function HeaderAnchor(props: { id: string }) {
+  return (
+    <a className="header-anchor" href={`#${props.id}`}>
+      <span className="sr-only">Jump to heading</span>
+      <span
+        aria-hidden="true"
+        class="anchor-end"
+      >
+        #
+      </span>
+    </a>
+  );
+}
