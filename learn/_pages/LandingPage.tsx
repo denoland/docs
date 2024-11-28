666f4a9afaf71016d9bab750c494ea0d59bc6f1b
Jo Franchetti <jofranchetti@gmail.com>
2024-11-28 23:48:41 +0000

    styling filter

--- learn/_pages/LandingPage.tsx
+++ learn/_pages/LandingPage.tsx
@@ -16,35 +16,54 @@ export default function LandingPage() {
   return (
     <main
       id="content"
-      className="w-full px-8 pt-6 mt-16 max-w-screen-xl mx-auto mb-20"
+      className="w-full px-8 mx-8 pt-6 mt-16 mb-20"
     >
-      <div className="w-full mb-8 flex">
+      <div className="w-full mb-8 flex justify-between gap-8
+      max-w-screen-xl mx-auto">
         <div className="max-w-prose">
           <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl mb-3">
             Learning hub
           </h1>
           <p>
-            Walkthrough tutorials, examples and guides to teach you about the
-            Deno runtime <br />and how to use it with your favorite tools.
+            A collection of walkthrough tutorials, examples, videos and guides
+            to teach you about the Deno runtime and how to use it with your
+            favorite tools.
           </p>
         </div>
-        <div>
-          <input
-            type="checkbox"
-            id="example"
-            value="Examples"
-            checked
-          />
-          <label for="example">Examples</label>
-          <input
-            type="checkbox"
-            id="tutorial"
-            value="Tutorials"
-            checked
-          />
-          <label for="tutorial">Tutorials</label>
-          <input type="checkbox" id="video" value="Videos" checked />
-          <label for="video">Videos</label>
+        <div className="mt-2 ml-5 border-l border-foreground-tertiary dark:border-background-tertiary py-4 pl-4">
+          <h2 className="text-lg font-semibold mb-2 whitespace-nowrap">
+            Filter by type
+          </h2>
+
+          <label for="example" className="block text-[13px]">
+            <input
+              type="checkbox"
+              id="example"
+              value="Examples"
+              className="mr-2"
+              checked
+            />Examples
+          </label>
+
+          <label for="tutorial" className="block text-[13px]">
+            <input
+              type="checkbox"
+              id="tutorial"
+              value="Tutorials"
+              className="mr-2"
+              checked
+            />Tutorials
+          </label>
+
+          <label for="video" className="block text-[13px]">
+            <input
+              type="checkbox"
+              id="video"
+              value="Videos"
+              className="mr-2"
+              checked
+            />Videos
+          </label>
         </div>
       </div>
       <div className="columns-1 sm:columns-2 lg:columns-3 gap-8">
