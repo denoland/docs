5f3ab97cc44e0a0fa86b0bcc29157a0695f60fa7
Jo Franchetti <jofranchetti@gmail.com>
2024-11-29 11:13:58 +0000

    Learning filter (#1210)

--- learn/_components/VideoIcon.tsx
+++ learn/_components/VideoIcon.tsx
@@ -2,17 +2,37 @@ export function VideoIcon(props: { color: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 445 348"
+      viewBox="0 0 416 368"
       className="inline mr-2"
       width="1rem"
     >
       <rect
-        x="15"
-        y="15"
-        width="415.3"
-        height="318"
-        rx="56.7"
-        ry="56.7"
+        x="25"
+        y="178"
+        width="274"
+        height="174.7"
+        rx="24.7"
+        ry="24.7"
+        fill="none"
+        stroke={props.color}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="30"
+      />
+      <circle
+        cx="71.2"
+        cy="122.2"
+        r="56.2"
+        fill="none"
+        stroke={props.color}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="30"
+      />
+      <circle
+        cx="229.7"
+        cy="96.7"
+        r="81.7"
         fill="none"
         stroke={props.color}
         stroke-linecap="round"
@@ -20,12 +40,12 @@ export function VideoIcon(props: { color: string }) {
         stroke-width="30"
       />
       <path
+        d="m306 246 85-53c4-3 10 0 10 5v135c0 5-6 8-10 6l-85-53"
         fill="none"
         stroke={props.color}
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke-width="30"
-        d="M15 95h415M67 18l74 75m165-78 72 73M228 15l70 70M145 15l77 77m-31 165v-92l98 46-98 46z"
       />
     </svg>
   );
--- learn/_pages/LandingPage.tsx
+++ learn/_pages/LandingPage.tsx
@@ -1,34 +1,88 @@
 import { LearningList } from "../_components/LearningList.tsx";
 import { sidebar } from "../tutorials/_data.ts";
+import { TutorialIcon } from "../_components/TutorialIcon.tsx";
+import { ExampleIcon } from "../_components/ExampleIcon.tsx";
+import { VideoIcon } from "../_components/VideoIcon.tsx";
 
 export default function LandingPage() {
-  const componentsPerSidebarItem = sidebar.map((item) => {
-    return (
-      <LearningList
-        title={item.title}
-        items={item.items}
-      />
-    );
-  });
+  const componentsPerSidebarItem = sidebar.map(
+    (item: { title: string; items: any[] }) => {
+      return (
+        <LearningList
+          title={item.title}
+          items={item.items}
+        />
+      );
+    },
+  );
 
   return (
     <main
       id="content"
-      className="w-full px-8 pt-6 mt-16 max-w-screen-xl mx-auto mb-20"
+      className="flex flex-col px-8 pt-6 md:pt-12 mt-4 md:items-center md:justify-center max-w-[1200px] mx-auto mb-48"
     >
-      <div className="w-full mb-8">
-        <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl mb-3">
-          Learning hub
-        </h1>
-        <p className="max-w-prose mb-6">
-          Walkthrough tutorials, examples and guides to teach you about the Deno
-          runtime <br />and how to use it with your favorite tools.
-        </p>
+      <div className="w-full mb-6 max-w-screen-xl mx-auto hub-header">
+        <div className="max-w-prose mb-8">
+          <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl mb-3">
+            Learning hub
+          </h1>
+          <p>
+            A collection of walkthrough tutorials, examples, videos and guides
+            to teach you about the Deno runtime and how to use it with your
+            favorite tools.
+          </p>
+        </div>
+        <div className=" mb-2 flex gap-2 flex-wrap items-center">
+        <h2 className="font-semibold mb-2 mt-2">
+          Filter by type:
+        </h2>
+          <label for="example" className="mr-4 ml-4 flex items-center">
+            <ExampleIcon color="#9d9d9d" />Examples:
+            <span className="switch">
+              <input
+                type="checkbox"
+                id="example"
+                value="Examples"
+                className="mr-2"
+                checked
+              />
+              <span className="slider"></span>
+            </span>
+          </label>
+
+          <label for="tutorial" className="mr-4 ml-4 flex items-center">
+            <TutorialIcon color="#9d9d9d" />Tutorials:
+            <span className="switch">
+              <input
+                type="checkbox"
+                id="tutorial"
+                value="Tutorials"
+                className="mr-2"
+                checked
+              />
+              <span className="slider"></span>
+            </span>
+          </label>
+
+          <label for="video" className="mr-4 ml-4 flex items-center">
+            <VideoIcon color="#9d9d9d" />Videos:
+            <span className="switch">
+              <input
+                type="checkbox"
+                id="video"
+                value="Videos"
+                className="mr-2"
+                checked
+              />
+              <span className="slider"></span>
+            </span>
+          </label>
+        </div>
       </div>
-      <div className="columns-1 sm:columns-2 lg:columns-3 gap-8">
+      <div className="columns-1 sm:columns-2 lg:columns-3 gap-8 mb-8">
         {componentsPerSidebarItem}
       </div>
-      <p class="max-w-prose mx-auto text-center">
+      <p class="max-w-prose mx-auto text-center pt-4 mt-3">
         Need an example that isn't here? Or want to add one of your own?<br />
         {" "}
         We welcome contributions!{" "}
