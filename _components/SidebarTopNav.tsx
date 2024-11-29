99753fd9a6f1da72f926946717e732679185f7b9
Jo Franchetti <jofranchetti@gmail.com>
2024-11-29 13:47:58 +0000

    style like homepage sidenav

--- _components/SidebarTopNav.tsx
+++ _components/SidebarTopNav.tsx
@@ -1,10 +1,11 @@
 export default function SidebarTopNav(props: { name: string; url: string }) {
   return (
-    <li>
-      <a href={props.url}>
-        <h2 class="border-b border-foreground-tertiary pt-2 pb-1.5 -mx-5 px-8 mt-4 mb-2 text-sm font-semibold hover:bg-background-secondary current:bg-background-secondary current:text-blue-500 text-foreground-primary capitalize">
-          {props.name}
-        </h2>
+    <li class="mx-2">
+      <a
+        class="block px-3 py-1.5 text-[.8125rem] leading-4 font-normal text-foreground-secondary rounded-md ring-1 ring-transparent hover:ring-background-tertiary hover:bg-background-secondary current:bg-background-secondary current:text-blue-500 current:font-semibold transition-colors duration-200 ease-in-out select-none"
+        href={props.url}
+      >
+        {props.name}
       </a>
     </li>
   );
