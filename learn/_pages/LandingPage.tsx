d7105051196c578ba08941608f243a42a94ad6f7
Jo Franchetti <jofranchetti@gmail.com>
2024-11-29 10:45:03 +0000

    style filters

--- learn/_pages/LandingPage.tsx
+++ learn/_pages/LandingPage.tsx
@@ -1,5 +1,8 @@
 import { LearningList } from "../_components/LearningList.tsx";
 import { sidebar } from "../tutorials/_data.ts";
+import { TutorialIcon } from "../_components/TutorialIcon.tsx";
+import { ExampleIcon } from "../_components/ExampleIcon.tsx";
+import { VideoIcon } from "../_components/VideoIcon.tsx";
 
 export default function LandingPage() {
   const componentsPerSidebarItem = sidebar.map(
@@ -16,11 +19,10 @@ export default function LandingPage() {
   return (
     <main
       id="content"
-      className="w-full px-8 mx-8 pt-6 mt-16 mb-20"
+      className="flex flex-col px-8 pt-6 md:pt-12 mt-4 md:items-center md:justify-center max-w-[1200px] mx-auto mb-48"
     >
-      <div className="w-full mb-8 flex justify-between gap-8
-      max-w-screen-xl mx-auto">
-        <div className="max-w-prose">
+      <div className="w-full mb-6 max-w-screen-xl mx-auto hub-header">
+        <div className="max-w-prose mb-8">
           <h1 className="text-2xl font-semibold sm:text-3xl md:text-4xl mb-3">
             Learning hub
           </h1>
@@ -30,46 +32,57 @@ export default function LandingPage() {
             favorite tools.
           </p>
         </div>
-        <div className="mt-2 ml-5 border-l border-foreground-tertiary dark:border-background-tertiary py-4 pl-4">
-          <h2 className="text-lg font-semibold mb-2 whitespace-nowrap">
-            Filter by type
-          </h2>
-
-          <label for="example" className="block text-[13px]">
-            <input
-              type="checkbox"
-              id="example"
-              value="Examples"
-              className="mr-2"
-              checked
-            />Examples
+        <h2 className="text-lg font-semibold mb-2 mt-2">
+          Filter by type:
+        </h2>
+        <div className=" mb-2 flex gap-8 flex-wrap">
+          <label for="example" className="mr-4 flex items-center">
+            <ExampleIcon color="#9d9d9d" />Examples:
+            <span className="switch">
+              <input
+                type="checkbox"
+                id="example"
+                value="Examples"
+                className="mr-2"
+                checked
+              />
+              <span className="slider"></span>
+            </span>
           </label>
 
-          <label for="tutorial" className="block text-[13px]">
-            <input
-              type="checkbox"
-              id="tutorial"
-              value="Tutorials"
-              className="mr-2"
-              checked
-            />Tutorials
+          <label for="tutorial" className="mr-4 flex items-center">
+            <TutorialIcon color="#9d9d9d" />Tutorials:
+            <span className="switch">
+              <input
+                type="checkbox"
+                id="tutorial"
+                value="Tutorials"
+                className="mr-2"
+                checked
+              />
+              <span className="slider"></span>
+            </span>
           </label>
 
-          <label for="video" className="block text-[13px]">
-            <input
-              type="checkbox"
-              id="video"
-              value="Videos"
-              className="mr-2"
-              checked
-            />Videos
+          <label for="video" className="mr-4 flex items-center">
+            <VideoIcon color="#9d9d9d" />Videos:
+            <span className="switch">
+              <input
+                type="checkbox"
+                id="video"
+                value="Videos"
+                className="mr-2"
+                checked
+              />
+              <span className="slider"></span>
+            </span>
           </label>
         </div>
       </div>
-      <div className="columns-1 sm:columns-2 lg:columns-3 gap-8">
+      <div className="columns-1 sm:columns-2 lg:columns-3 gap-8 mb-8">
         {componentsPerSidebarItem}
       </div>
-      <p class="max-w-prose mx-auto text-center">
+      <p class="max-w-prose mx-auto text-center pt-4 mt-3">
         Need an example that isn't here? Or want to add one of your own?<br />
         {" "}
         We welcome contributions!{" "}
