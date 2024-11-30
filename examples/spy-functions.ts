a138ca39790421c9f297f53802aece46ee8bf51e
adairo <adair.reyes45'@gmail.com>
2024-11-30 15:53:25 -0600

    refactor: apply deno fmt

--- examples/spy-functions.ts
+++ examples/spy-functions.ts
@@ -14,36 +14,36 @@
 import { assertSpyCall, assertSpyCalls, spy } from "jsr:@std/testing/mock";
 
 function log(message: string) {
-    console.log(message);
+  console.log(message);
 }
 
 const logger = { log };
 
 Deno.test("logger uses the log function", () => {
-    // Create a spy on the `logger.log` method.
-    const logSpy = spy(logger, "log");
+  // Create a spy on the `logger.log` method.
+  const logSpy = spy(logger, "log");
 
-    // Call the `logger.log` method.
-    logger.log("Hello, world!");
+  // Call the `logger.log` method.
+  logger.log("Hello, world!");
 
-    try {
-        // Assert that the `log` function was called just once.
-        assertSpyCalls(logSpy, 1);
+  try {
+    // Assert that the `log` function was called just once.
+    assertSpyCalls(logSpy, 1);
 
-        // Assert that the `log` function was called with the correct arguments.
-        assertSpyCall(logSpy, 0, { args: ["Hello, world!"] });
-    } finally {
-        // Restore the logger.log function to stop spying it.
-        logSpy.restore();
-    }
+    // Assert that the `log` function was called with the correct arguments.
+    assertSpyCall(logSpy, 0, { args: ["Hello, world!"] });
+  } finally {
+    // Restore the logger.log function to stop spying it.
+    logSpy.restore();
+  }
 });
 
 Deno.test("Creating a spy with the using keyword", () => {
-    // method spies are disposable, so we can create them with the keyword `using`
-    using logSpy = spy(logger, "log");
+  // method spies are disposable, so we can create them with the keyword `using`
+  using logSpy = spy(logger, "log");
 
-    logger.log("Disposable spy");
+  logger.log("Disposable spy");
 
-    // Spies created with the `using` keyword are automatically restored at the end of the test
-    assertSpyCalls(logSpy, 1);
+  // Spies created with the `using` keyword are automatically restored at the end of the test
+  assertSpyCalls(logSpy, 1);
 });
