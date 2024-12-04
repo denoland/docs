07429ee0d3e13047dd595a50cc01abd3162504bc
Yan Kuang <YTKme@Outlook.com>
2024-12-04 00:32:39 -0800

    Fix duckdb example syntax

--- examples/duckdb.ts
+++ examples/duckdb.ts
@@ -15,18 +15,18 @@
 import { open } from "https://deno.land/x/duckdb/mod.ts";
 
 // const db = open("./example.db");
-const db = open(':memory:');
+const db = open(":memory:");
 
 const connection = db.connect();
 
 const prepared = connection.prepare(
-  "SELECT ?::INTEGER AS number, ?::VARCHAR AS text",
+  "SELECT ?::INTEGER AS number, ?::VARCHAR AS text;",
 );
 
 const result = prepared.query(1337, "foo"); // [{ number: 1337, text: 'foo' }]
 
-console.debug(`Number: ${result[0].number}`)
-console.debug(`Text: ${result[0].text}`)
+console.debug(`Number: ${result[0].number}`);
+console.debug(`Text: ${result[0].text}`);
 
 connection.close();
 db.close();
