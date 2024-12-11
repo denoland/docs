a51593589716060ff19d1dac5189fb4b2e0b1032
Jo Franchetti <jofranchetti@gmail.com>
2024-12-11 16:07:36 +0000

    add totop

---
+++ _components/ToTop.tsx
@@ -0,0 +1,12 @@
+export default function ToTop() {
+  return (
+    <a href="#" className="top docs-cta runtime-cta">
+      <span>
+        &#8679;
+      </span>
+      <span>
+        To top
+      </span>
+    </a>
+  );
+}
