ba771306bb60821998f653decf9950d89f9b7359
Bartek Iwańczuk <biwanczuk@gmail.com>
2024-12-17 17:03:05 +0100

    add jsx tag

--- lint/index.tsx
+++ lint/index.tsx
@@ -1,37 +1,67 @@
 export const title = "Lint rules";
 
-
 export const toc = [];
 
-export default function(data: Lume.Data, _helpers: Lume.Helpers) {
-  console.log(data);
+const JsrLogo = () => (
+  <img src="/img/jsr.svg" className="lintRuleLogo" alt="jsr" />
+);
+
+const JsxLogo = () => (
+  <img src="/img/jsx.svg" className="lintRuleLogo" alt="jsx" />
+);
+
+const ReactLogo = () => (
+  <img src="/img/react.svg" className="lintRuleLogo" alt="react" />
+);
+
+const FreshLogo = () => (
+  <img src="/img/fresh.svg" className="lintRuleLogo" alt="fresh" />
+);
+
+const RecommendedLogo = () => <>✅</>;
+
+function getTag(tag: string) {
+  switch (tag) {
+    case "jsr":
+      return <JsrLogo />;
+    case "react":
+      return <ReactLogo />;
+    case "jsx":
+      return <JsxLogo />;
+    case "recommended":
+      return <RecommendedLogo />;
+    case "fresh":
+      return <FreshLogo />;
+    default:
+      break;
+  }
+}
+
+export default function (data: Lume.Data, _helpers: Lume.Helpers) {
   return (
     <div>
       <div class="lintRuleLogosGroup">
         <div>
-          ✅ = recommended
+          <RecommendedLogo /> = recommended
         </div>
         <div>
-          <img src="/img/fresh.svg" className="lintRuleLogo" alt="fresh" />{" "}
-          = fresh
+          <FreshLogo /> = fresh
         </div>
         <div>
-          <img src="/img/react.svg" className="lintRuleLogo" alt="react" />{" "}
-          = react
+          <JsxLogo /> = jsx
         </div>
         <div>
-          <img src="/img/jsr.svg" className="lintRuleLogo" alt="jsr" />{" "}
-          = jsr
+          <ReactLogo /> = react
+        </div>
+        <div>
+          <JsrLogo /> = jsr
         </div>
       </div>
-
 
       {data.lintRulePages.map((lintRule) => (
         <div>
           <a href={lintRule.href}>{lintRule.label}</a>{" "}
-          {lintRule.tags.map((tag) => (
-            <span className={`lint-tag-${tag}`}>{tag}</span>
-          ))}
+          {lintRule.tags.map((tag) => getTag(tag))}
         </div>
       ))}
     </div>
