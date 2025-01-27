b1545540b0950c5a1012a122b0cf7fc4ab51eeb4
crowlkats <crowlkats@toaxl.com>
2025-01-27 16:30:16 +0100

    fix

--- reference/reference.page.tsx
+++ reference/reference.page.tsx
@@ -28,60 +28,63 @@ const kinds = [
 ] as const;
 
 export default function* () {
-  if (Deno.env.has("SKIP_REFERENCE")) {
-    console.warn("⚠️ Reference docs generation was skipped.");
-    return;
-  }
+  try {
+    if (Deno.env.has("SKIP_REFERENCE")) {
+      throw new Error();
+    }
 
-  for (const { path, name } of kinds) {
-    const json = JSON.parse(Deno.readTextFileSync(path));
+    for (const { path, name } of kinds) {
+      const json = JSON.parse(Deno.readTextFileSync(path));
 
-    for (
-      const [filepath, content] of Object.entries(
-        json as Record<string, Page>,
-      )
-    ) {
-      if (content.kind === "search") {
-        continue;
-      }
+      for (
+        const [filepath, content] of Object.entries(
+          json as Record<string, Page>,
+        )
+      ) {
+        if (content.kind === "search") {
+          continue;
+        }
 
-      const trailingLength = filepath.endsWith("index.json")
-        ? -"index.json".length
-        : -".json".length;
+        const trailingLength = filepath.endsWith("index.json")
+          ? -"index.json".length
+          : -".json".length;
 
-      const url = `/api/${name.toLowerCase()}/${
-        filepath.slice(0, trailingLength)
-      }`;
+        const url = `/api/${name.toLowerCase()}/${
+          filepath.slice(0, trailingLength)
+        }`;
 
-      if ("path" in content) {
-        // TODO: handle redirects in a more integrated manner
+        if ("path" in content) {
+          // TODO: handle redirects in a more integrated manner
+
+          yield {
+            url,
+            content:
+              `<meta http-equiv="refresh" content="0; url=${content.path}">`,
+          };
+
+          continue;
+        }
+
+        let layout;
+        if (content.kind === "IndexCtx") {
+          layout = "index";
+        } else if (content.kind === "AllSymbolsCtx") {
+          layout = "allSymbols";
+        } else if (content.kind === "SymbolPageCtx") {
+          layout = "symbol";
+        } else {
+          throw `unknown page kind: ${content.kind}`;
+        }
 
         yield {
           url,
-          content:
-            `<meta http-equiv="refresh" content="0; url=${content.path}">`,
+          title: content.html_head_ctx.title,
+          layout: `reference/${layout}.tsx`,
+          data: content,
         };
-
-        continue;
       }
-
-      let layout;
-      if (content.kind === "IndexCtx") {
-        layout = "index";
-      } else if (content.kind === "AllSymbolsCtx") {
-        layout = "allSymbols";
-      } else if (content.kind === "SymbolPageCtx") {
-        layout = "symbol";
-      } else {
-        throw `unknown page kind: ${content.kind}`;
-      }
-
-      yield {
-        url,
-        title: content.html_head_ctx.title,
-        layout: `reference/${layout}.tsx`,
-        data: content,
-      };
     }
+  } catch (ex) {
+    console.warn("⚠️ Reference docs were not generated." + ex);
   }
 }
