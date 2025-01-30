4597ed8c0c05502c392e2cab37d04087e66d0fe8
Phil Hawksworth <phil@deno.com>
2025-01-30 16:16:26 +0000

    deno fmt

--- examples/scripts/postgres.ts
+++ examples/scripts/postgres.ts
@@ -9,11 +9,11 @@
  * running anywhere.
  */
 
-// Import the Postgres package from 
+// Import the Postgres package from
 import postgres from "npm:postgres";
 
 // Initialize the client with connection information for your database, and
-// create a connection. 
+// create a connection.
 const sql = postgres({
   user: "user",
   database: "test",
@@ -25,7 +25,7 @@ const sql = postgres({
 const result = await sql`
   SELECT ID, NAME FROM PEOPLE
 `;
-console.log(result); 
+console.log(result);
 
 // Close the connection to the database
 await sql.end();
