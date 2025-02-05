9495d138bd586c3e2cfc163b8685705d5a01ed74
Marvin Hagemeister <marvin@deno.com>
2025-02-05 21:40:58 +0100

    fix: dark mode lint rule border

--- lint/index.tsx
+++ lint/index.tsx
@@ -78,7 +78,7 @@ export default function LintRulesIndex(
       <ul class="flex flex-col gap-4 !list-none !pl-0">
         {data.lintRulePages.map((lintRule, idx: number) => (
           <li
-            class="border-t md:border md:rounded-md pt-8 pb-4 md:p-4 lint-rule-box"
+            class="border-t md:border md:rounded-md pt-8 pb-4 md:p-4 lint-rule-box dark:border-gray-700"
             id={lintRule.label}
           >
             <div class="flex flex-row justify-start items-center gap-4 mb-2">
