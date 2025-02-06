4facea16d70fb949795a5c19102259dcf15f5de6
Jo Franchetti <jofranchetti@gmail.com>
2025-02-06 14:27:04 +0000

    fmt

--- _components/Header.tsx
+++ _components/Header.tsx
@@ -90,7 +90,7 @@ export default function Header({
             external
             hideOnMobile
           />
-         <data.comp.SearchInput />
+          <data.comp.SearchInput />
           <data.comp.ThemeToggle />
         </div>
       </nav>
