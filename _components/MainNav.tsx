86655e997286c569054a503d2e4f95fd3e19e940
Jo Franchetti <jofranchetti@gmail.com>
2025-02-06 15:37:24 +0000

    adding components for new header (#1402)

---
+++ _components/MainNav.tsx
@@ -0,0 +1,23 @@
+import { NavData } from "../types.ts";
+
+export default function (data: Lume.Data) {
+  return (
+    <nav aria-labelledby="primary-navigation">
+      <ul className="main-nav">
+        {data.navigation.map((nav: NavData) => (
+          <li>
+            <a
+              href={nav.url}
+              className="main-nav-link"
+              data-active={data.currentUrl === nav.url}
+            >
+              {nav.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export const css = "@import './_components/MainNav.css';";
