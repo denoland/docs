40d1b233c2e28268b81956d4888fde188aae2161
Jo Franchetti <jofranchetti@gmail.com>
2025-02-10 12:42:07 +0000

    fmt

--- _includes/reference/base.tsx
+++ _includes/reference/base.tsx
@@ -9,35 +9,35 @@ export default function Base(
   return (
     <>
       <comp.RefNav data={data} url="/reference" />
-    <div className="ddoc">
-      <link rel="stylesheet" href="/reference_styles.css" />
-      <comp.CategoryPanel categoryPanel={data.categories_panel} />
+      <div className="ddoc">
+        <link rel="stylesheet" href="/reference_styles.css" />
+        <comp.CategoryPanel categoryPanel={data.categories_panel} />
 
-      <div>
-        <nav className="top-0 sticky bg-white z-50 py-3 h-14" id="topnav">
-          <div className="h-full">
-            <div>
-              <comp.Breadcrumbs parts={data.breadcrumbs_ctx.parts} />
+        <div>
+          <nav className="top-0 sticky bg-white z-50 py-3 h-14" id="topnav">
+            <div className="h-full">
+              <div>
+                <comp.Breadcrumbs parts={data.breadcrumbs_ctx.parts} />
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
 
-        <div id="content">
-          {children}
+          <div id="content">
+            {children}
 
-          <div>
-            {"toc_ctx" in data && (
-              <comp.Toc
-                documentNavigation={(data.toc_ctx as ToCCtx)
-                  .document_navigation}
-                documentNavigationStr={(data.toc_ctx as ToCCtx)
-                  .document_navigation_str}
-              />
-            )}
+            <div>
+              {"toc_ctx" in data && (
+                <comp.Toc
+                  documentNavigation={(data.toc_ctx as ToCCtx)
+                    .document_navigation}
+                  documentNavigationStr={(data.toc_ctx as ToCCtx)
+                    .document_navigation_str}
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
