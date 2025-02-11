c7f309186126f92fc915daec11e1e92ca3633038
Phil Hawksworth <phil@deno.com>
2025-02-11 09:13:56 +0000

    corrected js import filename

--- _components/VideoPlayer.tsx
+++ _components/VideoPlayer.tsx
@@ -1,5 +1,4 @@
 export const css = "@import './_components/VideoPlayer.css';";
-export const js = `import "/js/youtube-lite-client.js";`;
 
 /**
  * Embed a YouTube video.
@@ -35,3 +34,7 @@ function urlMatcher(url: string): string | undefined {
   const match = url.match(urlPattern);
   return match?.[3];
 }
+
+export const js = `
+import "/js/youtube-lite.client.js";
+`;
