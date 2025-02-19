5e6c363a2db4f12d4a268123c7fedbbc6e39e275
qti3e <i@parsa.ooo>
2025-02-19 18:10:05 -0500

    update style for text only snippets

--- examples/_components/SnippetComponent.tsx
+++ examples/_components/SnippetComponent.tsx
@@ -14,7 +14,7 @@ export default function SnippetComponent(props: {
     <div class="grid grid-cols-1 sm:grid-cols-10 gap-x-8">
       <div
         class={`italic select-none text-sm text-balance ${
-          props.snippet.text ? "pb-4 mt-4 md:pb-0 " : " "
+          props.snippet.text ? "pb-4 mt-4 " : " "
         } ${
           props.snippet.code ? "col-span-5 sm:col-span-3" : "mt-4 col-span-full"
         }`}
