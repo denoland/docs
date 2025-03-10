57e0031712e65839b016852da0ae0de757ae7eb0
Phil Hawksworth <phil@deno.com>
2025-03-10 15:15:44 +0000

    simplify styles build task

--- _config-styleguide.ts
+++ _config-styleguide.ts
@@ -1,17 +1,9 @@
 // Further refine the global Lume config for this section of the site
-import ogImages from "lume/plugins/og_images.ts";
 import site from "./_config.ts";
 
 // Ignore everything not in the styleguide folder
 site.ignore((path) => {
   return path.match(/^\/styleguide.*$/) === null;
 });
-
-site.data("openGraphLayout", "/open_graph/default.jsx");
-site.data("openGraphLayout", "/examples", "/open_graph/examples.jsx");
-// site.data("openGraphColor", "linear-gradient(-90deg, #70ffaf, #70ffaf 70%, white 70%)");
-site.data("openGraphColor", "#70ffaf");
-site.data("openGraphColor", "/examples", "#fff");
-site.use(ogImages());
 
 export default site;
