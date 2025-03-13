35e02fe97ff6ce5311079ecf97fc180b9112c996
Roy Revelt <roy@codsen.com>
2025-03-13 00:13:49 +0000

    fix: fix typo in functions name

--- runtime/_data.ts
+++ runtime/_data.ts
@@ -386,7 +386,7 @@ This the data is read from the files in the reference_gen/node_description direc
 This function is called in node.md through the templating engine Vento,
 after which the normal markdown rendered is called.
  */
-export async function generateNodeCompatability() {
+export async function generateNodeCompatibility() {
   const descriptions = await generateDescriptions();
   const sorted = Object.entries(descriptions).toSorted(([keyA], [keyB]) =>
     keyA.localeCompare(keyB)
