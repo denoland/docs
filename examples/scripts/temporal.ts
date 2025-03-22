1dcad4310844d6bf0c71fd04009df07365ed143e
Max Rashes <Mrashes2@gmail.com>
2025-03-22 22:55:41 +0000

    Formatter was grumpy
    This  fixes the format issues on the PR

--- examples/scripts/temporal.ts
+++ examples/scripts/temporal.ts
@@ -13,25 +13,25 @@ const date = Temporal.Now.plainDateISO(); // 2025-01-31
 
 // Return the date in ISO 8601 date format
 const dateAsString = date.toString(); // "2025-01-31"
-console.log(`Temporal date as string: ${dateAsString}`)
+console.log(`Temporal date as string: ${dateAsString}`);
 
 // Get current date and time in ISO 8601 format
 const plainDateTimeIsoString = Temporal.Now.plainDateTimeISO().toString(); // "2025-01-31T10:51:40.269979904"
-console.log(`Temporal plainDateTimeISO as string: ${plainDateTimeIsoString}`)
+console.log(`Temporal plainDateTimeISO as string: ${plainDateTimeIsoString}`);
 
 // Get Unix timestamp
 const timeStamp = Temporal.Now.instant(); // 2025-01-31T18:51:59.093355008Z
-console.log(`Temporal timestamp as string: ${timeStamp}`)
+console.log(`Temporal timestamp as string: ${timeStamp}`);
 
 // Return timestamp in milliseconds
 const epochMilliseconds = timeStamp.epochMilliseconds; // 1738349519093
-console.log(`Temporal timestamp epoch milliseconds: ${epochMilliseconds}`)
+console.log(`Temporal timestamp epoch milliseconds: ${epochMilliseconds}`);
 
 // Get date and time in ISO 8601 format from milliseconds
 const futureTime = Temporal.Instant.fromEpochMilliseconds(1851222399924); // 2028-08-30T04:26:39.924Z
-console.log(`Temporal future time: ${futureTime}`)
+console.log(`Temporal future time: ${futureTime}`);
 
 // Measure difference in hours from now.
 const now = Temporal.Now.instant();
 const differenceInHours = now.until(futureTime, { smallestUnit: "hour" }); // PT31600H
-console.log(`Temporal difference in hours: ${differenceInHours}`)
+console.log(`Temporal difference in hours: ${differenceInHours}`);
