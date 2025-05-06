45464354a940ea17ad93af68553f78611c2b94ab
Jo Franchetti <jofranchetti@gmail.com>
2025-05-05 21:48:53 -0500

    adding subnav

---
+++ _components/SubNav.tsx
@@ -0,0 +1,31 @@
+export default function (
+  { data, currentUrl }: {
+    data: Lume.Data;
+    currentUrl: string;
+  },
+) {
+  const navData = data.page?.data?.secondaryNav;
+  if (!navData || navData.length === 0) {
+    return null;
+  }
+
+  return (
+    <nav class="refheader">
+      <ul class="flex">
+        {navData.map((nav: any) => (
+          <li key={nav.href}>
+            <a
+              class="blocklink refheader-link"
+              data-active={currentUrl.includes(nav.href)}
+              href={nav.href}
+            >
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export const css = "@import './_components/RefHeader.css';";
