5b368073765d0cd0b5d124a902f9cf972c1bef2d
Jo Franchetti <jofranchetti@gmail.com>
2025-07-08 11:28:00 +0100

    adding component

---
+++ _components/DeployCTA.tsx
@@ -0,0 +1,12 @@
+export default function DeployCTA() {
+  return (
+    <a
+      href="https://app.deno.com/"
+      className="docs-cta deploy-cta deploy-button"
+    >
+      Deno Deploy
+    </a>
+  );
+}
+
+export const css = "@import './_components/CTA.css';";
