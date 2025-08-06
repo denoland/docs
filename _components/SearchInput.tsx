7eb4ea8f87bed4954723c33a0aeeddba7b2ba7af
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2025-08-06 16:37:12 +0200

    A couple more tiny UI fixes

--- _components/SearchInput.tsx
+++ _components/SearchInput.tsx
@@ -14,7 +14,7 @@ export default function SearchInput() {
           placeholder="Search"
           className="w-full min-w-24 rounded-lg text-sm leading-normal pt-1 pr-3 pb-1 pl-8 border transition-all duration-150
           text-foreground-primary border-foreground-tertiary hover:bg-background-secondary focus:bg-background-secondary"
-          style="background: url(/img/search.svg) no-repeat 0.5em 50%; background-size: 1em; background-color: hsl(var(--foreground-quaternary));"
+          style="background: url(/img/search.svg) no-repeat 0.5em 50%; background-size: 1em; background-color: var(--color-foreground-quaternary);"
         />
         <input type="hidden" name="q" id="q" value="site:deno.com" />
       </form>
