b8cc093550be96abc5fb379e2cb5416d00dc6551
Jo Franchetti <jofranchetti@gmail.com>
2025-08-21 16:18:49 +0100

    stop copying the deno json

--- _config.ts
+++ _config.ts
@@ -114,7 +114,6 @@ site.copy("runtime/contributing/images");
 site.copy("examples/tutorials/images");
 site.copy("deploy/manual/images");
 site.copy("deploy/early-access/images");
-site.copy("deno.json");
 site.copy("go.json");
 site.copy("oldurls.json");
 site.copy("server.ts");
