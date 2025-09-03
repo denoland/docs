6808bbd39528ee51d70a0d879e5bf56d21ccda02
Oscar Otero <oom@oscarotero.com>
2025-09-03 16:08:25 +0200

    fixed jsx configuration

--- _includes/open_graph/default.jsx
+++ _includes/open_graph/default.jsx
@@ -1,5 +1,3 @@
-/** @jsxImportSource npm:react@18.2.0 */
-
 export default function ({ title, description, openGraphColor }) {
   // Process text to replace superscript EA with "Early Access"
   const processEarlyAccessText = (text) => {
