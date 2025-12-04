39543334bed95524850d3aed62763be43cc7b371
Phil Hawksworth <phil@deno.com>
2025-12-04 12:09:30 +0000

    add sandbox example layout

---
+++ _includes/sandbox-example.tsx
@@ -0,0 +1,21 @@
+export const layout = "doc.tsx";
+
+export default function Raw(data: Lume.Data) {
+  return (
+    <>
+      <p className="italic">
+        <a href="https://deno.com/deploy/sandbox/">Deno Sandboxes</a>{" "}
+        provide a sandboxed environment for evaluating JavaScript code. This is
+        useful for evaluating code that is not trusted or for testing code that
+        is not safe to run in the main runtime.
+      </p>
+
+      {data.children}
+
+      <p className="my-8 block">
+        For more information about Sandboxes, see the{" "}
+        <a href="/sandboxes/">Sandboxes documentation</a>.
+      </p>
+    </>
+  );
+}
