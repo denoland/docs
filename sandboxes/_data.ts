c9de48dac9b515022262a00c80c4b77f7ff3a5a5
Phil Hawksworth <phil@deno.com>
2025-12-05 12:18:52 +0000

    Add structure to sidebar

--- sandboxes/_data.ts
+++ sandboxes/_data.ts
@@ -9,23 +9,19 @@ export const sidebar = [
         title: "Getting started",
         href: "/sandboxes/getting_started/",
       },
-    ],
-  },
-  {
-    title: "Reference",
-    items: [
       {
         title: "Create a sandbox",
         href: "/sandboxes/reference/create/",
       },
       {
-        title: "Manage sandboxes",
-        href: "/sandboxes/reference/manage/",
-      },
-      {
         title: "Promoting sandboxes",
         href: "/sandboxes/reference/promote/",
       },
+    ],
+  },
+  {
+    title: "Concepts",
+    items: [
       {
         title: "Expose HTTP",
         href: "/sandboxes/reference/expose_http/",
@@ -39,8 +35,21 @@ export const sidebar = [
         href: "/sandboxes/reference/lifetimes/",
       },
       {
+        title: "Manage Deploy apps",
+        href: "/sandboxes/reference/manage_apps/",
+      },
+    ],
+  },
+  {
+    title: "Reference",
+    items: [
+      {
         title: "API Reference",
         href: "https://jsr.io/@deno/sandbox/doc/~/Sandbox",
+      },
+      {
+        title: "CLI subcommand",
+        href: "/runtime/reference/cli/deploy/#sandbox",
       },
       {
         title: "Examples",
