f8f098e327189fea549d9c4f2d7e8065852f2596
Ryan Dahl <ry@tinyclouds.org>
2026-02-02 08:17:03 -0800

    Add docs for snapshots (#2877)

--- sandbox/_data.ts
+++ sandbox/_data.ts
@@ -35,7 +35,7 @@ export const sidebar = [
         href: "/sandbox/apps/",
       },
       {
-        title: "Volumes",
+        title: "Volumes & Snapshots",
         href: "/sandbox/volumes/",
       },
       {
