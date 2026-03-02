aea4e331b297a6796a68a59d9de0a182c93bdc15
John Donmoyer <john@deno.com>
2026-03-02 13:19:36 -0500

    style: reduce copy button icon size from 14px to 12px

--- _components/CopyPage.tsx
+++ _components/CopyPage.tsx
@@ -19,8 +19,8 @@ export default function CopyPage({ file }: { file: string | undefined }) {
           xmlns="http://www.w3.org/2000/svg"
           aria-hidden="true"
           fill="currentColor"
-          width="14"
-          height="14"
+          width="12"
+          height="12"
           viewBox="0 0 16 16"
         >
           <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z" />
