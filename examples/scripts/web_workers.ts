caa9f751fae5c340e078843918893eaebe632473
Yusuke Tanaka <wing0920@gmail.com>
2026-08-17 20:51:31 +0900

    fix(examples): relabel worker resource link

--- examples/scripts/web_workers.ts
+++ examples/scripts/web_workers.ts
@@ -3,7 +3,7 @@
  * @difficulty intermediate
  * @tags cli, web
  * @resource {https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers} MDN: Web Workers
- * @resource {https://docs.deno.com/api/web/~/Worker/} Manual: Workers
+ * @resource {https://docs.deno.com/api/web/~/Worker/} API: Worker
  * @group Advanced
  *
  * Workers are the only way of running javascript off of the main thread.
